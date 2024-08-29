5ee175072082f4f7b0330d6b8d6ca7c33090ef1d
hyunmin-ooo <2201110357@office.kopo.ac.kr>
2024-08-29 11:49:08 +0900

    7장 객체지향1,2

---
+++ src/cat/Main.java
@@ -0,0 +1,22 @@
+package cat;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.setName("AICat");
+        cat.setBreeds("persian");
+        cat.setWeight(3);
+
+        System.out.printf("이름: %s\n", cat.getName());
+        System.out.printf("품종: %s\n", cat.getBreeds());
+        System.out.printf("체중: %.2fkg\n", cat.getWeight());
+
+        System.out.print(cat.getName()+"이 ");
+        cat.claw();
+
+        System.out.print(cat.getName()+"이 ");
+        cat.meow();
+    }
+}
